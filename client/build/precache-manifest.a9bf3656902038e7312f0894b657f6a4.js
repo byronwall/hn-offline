414014102ace4b2f218a8c2dd50e6523309180de
Byron Wall <byron@byroni.us>
2020-05-25 00:08:26 -0400

    Update build

---
+++ client/build/precache-manifest.a9bf3656902038e7312f0894b657f6a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79516a9ab28d9c249d9850d676f207e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d6b0d19b8034af76ca3",
+    "url": "/static/css/2.29c12da1.chunk.css"
+  },
+  {
+    "revision": "b2fa8db31263e032611f",
+    "url": "/static/css/main.88856e57.chunk.css"
+  },
+  {
+    "revision": "4d6b0d19b8034af76ca3",
+    "url": "/static/js/2.94c584b9.chunk.js"
+  },
+  {
+    "revision": "6bfeab3bf42cb8cac2d0eca2a9883324",
+    "url": "/static/js/2.94c584b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2fa8db31263e032611f",
+    "url": "/static/js/main.e76b542e.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
