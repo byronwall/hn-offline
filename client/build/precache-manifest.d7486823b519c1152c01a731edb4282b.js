0fa70f99916b3b11b9355feb5933dcdc475088db
Byron Wall <byron@byroni.us>
2020-07-01 22:34:59 -0400

    Update build

--- client/build/precache-manifest.d7486823b519c1152c01a731edb4282b.js
+++ client/build/precache-manifest.d7486823b519c1152c01a731edb4282b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c08b72423c6b4214a5235da5fe8410b",
+    "revision": "c79b74cb1b923cac98198912bc417453",
     "url": "/index.html"
   },
   {
-    "revision": "ebe1f1fc566df4cc7cd2",
+    "revision": "6babaedd6ba3e804f02c",
     "url": "/static/css/2.92142000.chunk.css"
   },
   {
-    "revision": "8deb0b649da08334b176",
+    "revision": "64b3bbf408e7a7522978",
     "url": "/static/css/main.98eebecd.chunk.css"
   },
   {
-    "revision": "ebe1f1fc566df4cc7cd2",
-    "url": "/static/js/2.6d8704c5.chunk.js"
+    "revision": "6babaedd6ba3e804f02c",
+    "url": "/static/js/2.8b1299e3.chunk.js"
   },
   {
     "revision": "a4d1ddcae88812943d8bd3e82866f499",
-    "url": "/static/js/2.6d8704c5.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8b1299e3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8deb0b649da08334b176",
-    "url": "/static/js/main.421578db.chunk.js"
+    "revision": "64b3bbf408e7a7522978",
+    "url": "/static/js/main.c434407c.chunk.js"
   },
   {
     "revision": "13c5ce7c89dba05c4fba",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0915af3315ab3720305768e64a96203c.js"
+  "/precache-manifest.d7486823b519c1152c01a731edb4282b.js"
 );
 
 self.addEventListener('message', (event) => {
