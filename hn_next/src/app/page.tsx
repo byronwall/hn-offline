8483a4bfe44342a97c174f7cb6cc146e4355f0b6
Byron Wall <byron@byroni.us>
2024-03-03 00:38:49 -0500

    Update main page

--- hn_next/src/app/page.tsx
+++ hn_next/src/app/page.tsx
@@ -1,9 +1,10 @@
-import StoryListPage from "./[datatype]/page";
+import { StoryListPage } from "@/components/StoryListPage";
+import { getSummaryViaFetch } from "@/stores/getSummaryViaFetch";
 
-export default function Home() {
-  return <StoryListPage
-    params={{
-      datatype: "topstories"
-    }}
-  />;
+export default async function Home() {
+  const { data } = await getSummaryViaFetch("/api/topstories/topstories");
+
+  console.log("ssr data @ server", "/ front", data.length);
+
+  return <StoryListPage data={data} />;
 }
