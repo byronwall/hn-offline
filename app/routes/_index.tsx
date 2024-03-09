c4af52761c51eeb5bdf4e31d08b2000026f9dcea
Byron Wall <byron@byroni.us>
2024-03-08 22:49:51 -0500

    Move meta to main page (#36)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,36 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
 import { ClientLoaderFunctionArgs } from "@remix-run/react";
 
 import HnStoryListServer, {
-  loader as listLoader,
   clientLoader as commonClientLoader,
+  loader as listLoader,
 } from "./$type";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "HN Offline" },
-    {
-      name: "description",
-      content: "Hacker News client built for offline usage",
-    },
-    {
-      name: "manifest",
-      content: "/manifest.json",
-    },
-    {
-      name: "apple-mobile-web-app-capable",
-      content: "yes",
-    },
-    {
-      name: "apple-mobile-web-app-status-bar-style",
-      content: "black",
-    },
-    {
-      name: "apple-mobile-web-app-title",
-      content: "HN Offline",
-    },
-  ];
-};
 
 export async function loader() {
   return listLoader({ params: { type: "topstories" } });
