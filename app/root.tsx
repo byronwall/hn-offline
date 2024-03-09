354eba3eb45efad5ef52ec22e4dd6bd2e95f579b
Byron Wall <byron@byroni.us>
2024-03-08 23:54:48 -0500

    Give correct manifest tag

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,7 @@ export default function App() {
           name="description"
           content="Hacker News client built for offline usage"
         />
-        <meta name="manifest" content="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="HN Offline" />
