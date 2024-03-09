94df36a6c013b35702021c7175085ea7d4ffe0c6
Byron Wall <byron@byroni.us>
2024-03-08 22:25:03 -0500

    Remove next

--- hn_next/src/stores/ensureUrlIsFullyQualified.ts
+++
@@ -1,7 +0,0 @@
-export function ensureUrlIsFullyQualified(url: string) {
-  const publicPrefix = process.env.NEXT_PUBLIC_SITE_URL || "";
-  if (!url.startsWith(publicPrefix)) {
-    url = publicPrefix + url;
-  }
-  return url;
-}
