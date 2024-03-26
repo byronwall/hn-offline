fe017a77689c792f981f781b1357f763fa270d02
Byron Wall <byron@byroni.us>
2024-03-25 22:36:37 -0400

    Add meta for comment only links

--- app/routes/story.$id.tsx
+++ app/routes/story.$id.tsx
@@ -6,15 +6,37 @@ import { getDomain } from "~/utils";
 
 import { loader as storyLoader } from "./api.story.$id";
 
-export const meta: MetaFunction<typeof loader> = ({ data }) => [
-  { title: "HN Offline: " + data.title },
-  {
-    name: "description",
-    content: `${data.score} points at ${getDomain(data.url)} by ${data.by} - ${
-      data.descendants
-    } comments`,
-  },
-];
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (!data || "error" in data) {
+    return [
+      { title: "HN Offline" },
+      {
+        name: "description",
+        content: "Hacker News Offline",
+      },
+    ];
+  }
+
+  if (data.type === "comment") {
+    return [
+      { title: "HN Offline Comment by " + data.by },
+      {
+        name: "description",
+        content: `${data.text}`,
+      },
+    ];
+  }
+
+  return [
+    { title: "HN Offline: " + data.title },
+    {
+      name: "description",
+      content: `${data.score} points at ${getDomain(data.url)} by ${
+        data.by
+      } - ${data.descendants} comments`,
+    },
+  ];
+};
 
 export async function loader({ params }: LoaderFunctionArgs) {
   // this action will run only on a SSR request - direct load of URL
