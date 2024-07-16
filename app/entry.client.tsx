31ac3df3f41dcdb74f35bfdf4da8be68a78760f2
Byron Wall <byron@byroni.us>
2024-07-15 23:44:52 -0400

    Add posthog

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,10 +8,16 @@ import { RemixBrowser } from "@remix-run/react";
 import * as Sentry from "@sentry/remix";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
+import posthog from "posthog-js";
 
 // TODO: make this an environment variable
 Sentry.init({
   dsn: "https://3b80771b89684f458ac049432eaf81b5@occ4w04.apps.byroni.us/1",
+});
+
+posthog.init("phc_z3tWe9VlG3FBekdNNnT2La18sXvHFt7s6yTB8Gk5p8R", {
+  api_host: "https://web-nwk8g88.apps.byroni.us",
+  person_profiles: "always", // or 'always' to create profiles for anonymous users as well
 });
 
 // load service worker @ sw.js
