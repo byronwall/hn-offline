8ca5eeb01100b8f84ed7913e2bee05147574985f
Byron Wall <byron@byroni.us>
2024-11-28 13:32:13 -0500

    Adjust border color to match poster name

---
+++ app/components/stringToColor.tsx
@@ -0,0 +1,36 @@
+const colorMap: Record<string, string> = {};
+
+export function stringToColor(
+  str: string | undefined,
+  isCommentByStoryAuthor: boolean
+) {
+  if (str === undefined) {
+    return "#000000";
+  }
+
+  if (isCommentByStoryAuthor) {
+    return "#f97316"; // orange color
+  }
+
+  if (colorMap[str]) {
+    return colorMap[str];
+  }
+
+  const minLightness: number = 50; // Increased to avoid dark colors
+  const maxLightness: number = 80;
+
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const h = hash % 360;
+  const s = 70 + (hash % 30); // Saturation between 70% and 100%
+  const l = minLightness + (hash % (maxLightness - minLightness)); // Lightness between minLightness and maxLightness
+
+  const color = `hsl(${h}, ${s}%, ${l}%)`;
+
+  colorMap[str] = color;
+
+  return color;
+}
