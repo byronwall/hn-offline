007f6292f771701ce9f18397d2ab30e094ffa51a
Byron Wall <byron@byroni.us>
2024-12-02 21:40:25 -0500

    Add text when list is empty (#70)

--- app/components/HnStoryList.tsx
+++ app/components/HnStoryList.tsx
@@ -21,6 +21,15 @@ export function HnStoryList({ items, sortType, page }: HnStoryListProps) {
     return <div>Loading...</div>;
   }
 
+  if (itemsToRender.length === 0) {
+    return (
+      <div className="text-center text-gray-500 text-lg">
+        No items to show. Most likely, you are filtering to hide read items.
+        Click the toggle above to change the setting.
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="grid grid-cols-[1fr_1fr_1fr_3fr]">
