7c4cb5f8485898b92bdb99848c533d076510110a
Byron Wall <byron@byroni.us>
2024-12-09 21:57:09 -0500

    Ensure negative margin is applied only when open

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v27";
-const RUNTIME = "runtime-v27";
+const PRECACHE = "precache-v28";
+const RUNTIME = "runtime-v28";
 
 // A list of local resources we always want to be cached.
 // need these available so the site can work offline on all major pages
