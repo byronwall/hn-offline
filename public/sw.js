74a805bd9bb49effcbef9ffd86617787d0a9ed03
Byron Wall <byron@byroni.us>
2024-12-10 23:55:51 -0500

    Revise scroll logic on collapse (#85)
    Roll to the next parent instead of current parent

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v29";
-const RUNTIME = "runtime-v29";
+const PRECACHE = "precache-v30";
+const RUNTIME = "runtime-v30";
 
 // A list of local resources we always want to be cached.
 // need these available so the site can work offline on all major pages
