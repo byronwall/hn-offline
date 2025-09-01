8d55763505f96d31af177b094bf160be868a6e91
Byron Wall <byron@byroni.us>
2025-08-31 22:28:54 -0400

    Add apple touch icon link in entry-server.tsx for improved mobile support

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -61,6 +61,7 @@ export default createHandler(() => (
             />
             <link rel="icon" href="/favicon.ico" />
             <link rel="manifest" href="/manifest.webmanifest" />
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta
               name="apple-mobile-web-app-status-bar-style"
