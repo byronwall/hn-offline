b261b87c99fdf090205d551514d6dd0d4c897398
Byron Wall <byron@byroni.us>
2025-09-01 21:49:45 -0400

    Add PullToRefresh component to HnStoryPage for improved user experience during data refresh. Disable refresh functionality based on loading state and offline mode.

--- src/features/comments/HnStoryPage.tsx
+++ src/features/comments/HnStoryPage.tsx
@@ -2,16 +2,19 @@ import { useNavigate } from "@solidjs/router";
 import { Match, onCleanup, onMount, Show, Switch } from "solid-js";
 
 import { ArrowUpRightFromSquare } from "~/components/Icon";
+import { PullToRefresh } from "~/components/PullToRefresh";
 import { createHasRendered } from "~/lib/createHasRendered";
 import { isValidComment } from "~/lib/isValidComment";
 import { processHtmlAndTruncateAnchorText } from "~/lib/processHtmlAndTruncateAnchorText";
 import { cn, getDomain, timeSince } from "~/lib/utils";
 import { activeStoryData } from "~/stores/activeStorySignal";
 import { setScrollToId } from "~/stores/scrollSignal";
+import { isOfflineMode } from "~/stores/serviceWorkerStatus";
 import {
   collapsedTimestamps,
   updateCollapsedState,
 } from "~/stores/useCommentStore";
+import { isLoadingData, refreshActive } from "~/stores/useDataStore";
 import { saveIdToReadList } from "~/stores/useReadItemsStore";
 
 import { HnCommentList } from "./HnCommentList";
@@ -114,58 +117,67 @@ export const HnStoryPage = (props: HnStoryPageProps) => {
   }
 
   return (
-    <div class="relative pb-[70vh]">
-      <h2
-        class="mb-2 text-2xl font-bold hover:underline"
-        style={{ "overflow-wrap": "break-word" }}
-      >
-        <Switch>
-          <Match when={activeStoryData()?.url === undefined}>
-            <span>{activeStoryData()?.title}</span>
-          </Match>
-          <Match when={activeStoryData()?.url !== undefined}>
-            <a href={activeStoryData()?.url}>{activeStoryData()?.title}</a>
-          </Match>
-        </Switch>
-      </h2>
+    <PullToRefresh
+      disabled={isLoadingData() || isOfflineMode()}
+      onRefresh={refreshActive}
+    >
+      <div class="relative pb-[70vh]">
+        <h2
+          class="mb-2 text-2xl font-bold hover:underline"
+          style={{ "overflow-wrap": "break-word" }}
+        >
+          <Switch>
+            <Match when={activeStoryData()?.url === undefined}>
+              <span>{activeStoryData()?.title}</span>
+            </Match>
+            <Match when={activeStoryData()?.url !== undefined}>
+              <a href={activeStoryData()?.url}>{activeStoryData()?.title}</a>
+            </Match>
+          </Switch>
+        </h2>
 
-      <div
-        class={cn({
-          "rounded-tl rounded-bl border-l-4 border-orange-500 px-2":
-            activeStoryData()?.text,
-          collapsed: !isTextOpen(),
-        })}
-        onClick={handleStoryTextClick}
-      >
-        <h4 class="mb-2">
-          <span>{activeStoryData()?.by}</span>
-          <span>{" | "}</span>
-          <span>
-            {activeStoryData()?.score}
-            {" points"}
-          </span>
-          <span>{" | "}</span>
-          <span>{timeSince(activeStoryData()?.time)} ago</span>
-          <span>{" | "}</span>
-          <span>{getDomain(activeStoryData()?.url)}</span>
+        <div
+          class={cn({
+            "rounded-tl rounded-bl border-l-4 border-orange-500 px-2":
+              activeStoryData()?.text,
+            collapsed: !isTextOpen(),
+          })}
+          onClick={handleStoryTextClick}
+        >
+          <h4 class="mb-2">
+            <span>{activeStoryData()?.by}</span>
+            <span>{" | "}</span>
+            <span>
+              {activeStoryData()?.score}
+              {" points"}
+            </span>
+            <span>{" | "}</span>
+            <span>{timeSince(activeStoryData()?.time)} ago</span>
+            <span>{" | "}</span>
+            <span>{getDomain(activeStoryData()?.url)}</span>
 
-          <span>{" | "}</span>
-          <button onClick={handleShareClick} class="hover:text-orange-500">
-            <ArrowUpRightFromSquare size={16} />
-          </button>
-        </h4>
+            <span>{" | "}</span>
+            <button onClick={handleShareClick} class="hover:text-orange-500">
+              <ArrowUpRightFromSquare size={16} />
+            </button>
+          </h4>
 
-        <Show when={activeStoryData()?.text !== undefined && isTextOpen()}>
-          <div>
-            {/*  eslint-disable-next-line solid/no-innerhtml */}
-            <p class="user-text break-words" innerHTML={textToRender()} />
-          </div>
-        </Show>
+          <Show when={activeStoryData()?.text !== undefined && isTextOpen()}>
+            <div>
+              {/*  eslint-disable-next-line solid/no-innerhtml */}
+              <p class="user-text break-words" innerHTML={textToRender()} />
+            </div>
+          </Show>
+        </div>
+
+        <div class="user-text">
+          <HnCommentList
+            childComments={comments()}
+            depth={0}
+            authorChain={[]}
+          />
+        </div>
       </div>
-
-      <div class="user-text">
-        <HnCommentList childComments={comments()} depth={0} authorChain={[]} />
-      </div>
-    </div>
+    </PullToRefresh>
   );
 };
