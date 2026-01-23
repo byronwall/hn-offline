efe13bd3204f7d0415baf6d4799c685efde5f14f
Byron Wall <byron@byroni.us>
2026-01-23 07:33:42 -0500

    Remove deprecated API routes for clearing the database and fetching stories, streamlining the codebase by eliminating unused files.

--- src/routes/api/topstories/[type].ts
+++
@@ -1,7 +0,0 @@
-import { getTopStories } from "~/server/getTopStories";
-
-import type { APIEvent } from "@solidjs/start/server";
-
-export const GET = async ({ params }: APIEvent) => {
-  return getTopStories(params.type);
-};
