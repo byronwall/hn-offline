f604f74c911ca9e9f225767c4fb238af2f10d090
Byron Wall <byron@byroni.us>
2026-01-30 23:56:23 -0500

    Resolve issues with persisting client data for story list

--- src/features/storyList/ServerStoryList.tsx
+++ src/features/storyList/ServerStoryList.tsx
@@ -1,26 +1,21 @@
-import { createAsync } from "@solidjs/router";
-import { createEffect, createMemo, untrack } from "solid-js";
-import { createStore, reconcile } from "solid-js/store";
+import { createEffect, createResource, untrack } from "solid-js";
 
 import {
   updateStoryListDataStores,
   useAppData,
   useDataStore,
 } from "~/contexts/AppDataContext";
-import { mapStoriesToSummaries } from "~/lib/getSummaryViaFetch";
 import { TopStoriesType } from "~/models/interfaces";
 import { getStoryListByType } from "~/server/queries";
 
 import { HnStoryListBody } from "./HnStoryListBody";
 import { HnStoryListToggle } from "./HnStoryListToggle";
 
-import type { HnStorySummary } from "~/models/interfaces";
-
 export function ServerStoryList(props: { page: TopStoriesType }) {
   const isClientMounted = useAppData().isClientMounted;
   const dataStore = useDataStore();
 
-  const data = createAsync(async () => {
+  const [data] = createResource(async () => {
     const isClient = untrack(() => isClientMounted());
 
     if (isClient) {
@@ -29,28 +24,7 @@ export function ServerStoryList(props: { page: TopStoriesType }) {
       return { result: list, startedFromServer: false };
     }
 
-    return getStoryListByType(props.page);
-  });
-
-  const summaries = createMemo(() => {
-    const fromQuery = data.latest?.result;
-    if (fromQuery && fromQuery.type === "fullData") {
-      // we need to map the full data to summaries
-      return mapStoriesToSummaries(fromQuery.data);
-    }
-    return fromQuery?.data;
-  });
-
-  const [summaryStore, setSummaryStore] = createStore<HnStorySummary[]>(
-    data.latest?.result?.data ?? []
-  );
-
-  createEffect(() => {
-    const sums = summaries();
-    if (sums) {
-      // in theory this store should give a nice reorder effect for auto-animate?
-      setSummaryStore(reconcile(sums, { key: "id" }));
-    }
+    return await getStoryListByType(props.page);
   });
 
   createEffect(() => {
@@ -59,7 +33,7 @@ export function ServerStoryList(props: { page: TopStoriesType }) {
 
   return (
     <>
-      <HnStoryListBody items={summaryStore} page={props.page} />
+      <HnStoryListBody items={data()?.result?.data ?? []} page={props.page} />
       <HnStoryListToggle />
     </>
   );
